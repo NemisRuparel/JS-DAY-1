6448ccd105ddf00905fcbb5c3819ef751c128689
Nemis Ruparel <nemisruparel07@gmail.com>
2025-01-11 22:25:48 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+var a=8,b=0;
+var c=a/b;
+var text = "Mars is known as \"Red Plannet\"";
+document.write("<h1>Answer of 8 / 0 is: " + c + "</h1>");
+document.write(Number.MIN_VALUE);
+
