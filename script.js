c297418af1e6992f7afc02f88a9a8d86e1b71c81
Nemis Ruparel <nemisruparel07@gmail.com>
2025-01-20 21:54:13 +0530

    JS Day-2

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-console.log("Hello World");
-
-var a=8,b=0;
-var c=a/b;
-var text = "Mars is known as \"Red Plannet\"";
-document.write("<h1>Answer of 8 / 0 is: " + c + "</h1>");
-document.write(Number.MIN_VALUE);
-
+var paragraphs = document.getElementsByTagName("p");
+for(paragraph of paragraphs){
+    paragraph.style.background = "red";
+}
